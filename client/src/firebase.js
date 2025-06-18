4df38731a6f2d13f1654dc25541928bddd75f06f
N_VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-06-18 22:40:52 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -7,13 +10,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDEu3iJYfwI-eUGwX-mPxHcCVn9uJEK8kg",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: "famtree-34bc8.firebaseapp.com",
     projectId: "famtree-34bc8",
     storageBucket: "famtree-34bc8.firebasestorage.app",
-    messagingSenderId: "288867716714",
-    appId: "1:288867716714:web:8d36327b1fe7862c4a8413",
-    measurementId: "G-K1FT3R3VFH"
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
