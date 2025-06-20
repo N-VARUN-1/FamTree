21910e9864c49d5bdd8abb84f3c2020c96b008d3
N_VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-06-20 13:25:31 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,37 +1,3 @@
-// import express from 'express';
-// import dotenv from 'dotenv';
-// import cors from 'cors';
-
-// dotenv.config();
-
-// const app = express();
-
-// // ✅ use cors middleware
-// app.use(cors({
-//   origin: 'https://fam-tree-frontend.vercel.app',
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-
-// // Explicitly handle OPTIONS requests for all routes
-// app.options('*', cors());
-
-
-// app.use(express.json());
-
-// import userRoutes from './routes/user.route.js';
-// import passRoute from './routes/passRoute/pass.route.js';
-
-// app.use('/api', userRoutes);
-// app.use('/api', passRoute);
-
-// app.listen(3000, () => {
-//     console.log("Server is running on port 3000");
-// });
-
-
-
 
 import express from 'express';
 import dotenv from 'dotenv';
