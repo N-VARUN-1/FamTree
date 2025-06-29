0e2362cf9f2dbf8687768d31b8e59797a3fd968d
N_VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-06-29 12:34:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ const corsOptions = {
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: true,
   optionsSuccessStatus: 200 
 };
 
