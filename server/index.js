77d6e556973aea43e0b7dbab0ebf17ead73d5217
N_VARUN <159157923+N-VARUN-1@users.noreply.github.com>
2025-07-03 13:09:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', 'https://fam-tree-frontend.vercel.app');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     return res.status(200).end();
   }
